a84b211552989360c05829dc5c70c7e67605bf25
Shamaralyy <2670602083@qq.com>
2023-08-24 12:08:04 +0800

    fix: uploadFile API modify

--- src/api/setFile/setFile.ts
+++ src/api/setFile/setFile.ts
@@ -2,14 +2,11 @@ import request from "@/utils/request";
 
 export function uploadFile(psw: string, file: any) {
     return request({
-        url: '/uploadFile',
+        url: '/uploadFile?password='+psw,
         method: 'post',
-        // headers: {
-        //     'Content-Type': 'multipart/form-data'
-        // },
-        data: {
-            file,
-            psw
-        }
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        },
+        data: file,
     })
 }
