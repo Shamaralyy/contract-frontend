7e153eb6431eb10bad4488f87e26177b9a460a18
Shamaralyy <2670602083@qq.com>
2023-09-10 13:16:42 +0800

    fix: 转base64修改

--- src/utils/base64ToUrl.ts
+++ src/utils/base64ToUrl.ts
@@ -6,13 +6,17 @@ export function getBase64URL(pic) {
 
 export function base64ImgtoFile (dataurl, filename = 'file') {
     //将base64格式分割：['data:image/png;base64','XXXX']
-    const arr = dataurl.split(',')
+    // const arr = dataurl.split(',')
+    
     // .*？ 表示匹配任意字符到下一个符合条件的字符 刚好匹配到：
     // image/png
-    const mime = arr[0].match(/:(.*?);/)[1]  //image/png
+    // const mime = arr[0].match(/:(.*?);/)[1]  //image/png
+    const mime = 'image/png';
     //[image,png] 获取图片类型后缀
-    const suffix = mime.split('/')[1] //png
-    const bstr = atob(arr[1])   //atob() 方法用于解码使用 base-64 编码的字符串
+    // const suffix = mime.split('/')[1] //png
+    const suffix = 'png';
+    // const bstr = atob(arr[1])   //atob() 方法用于解码使用 base-64 编码的字符串
+    const bstr = atob(dataurl)   //atob() 方法用于解码使用 base-64 编码的字符串
     let n = bstr.length
     const u8arr = new Uint8Array(n)
     while (n--) {
